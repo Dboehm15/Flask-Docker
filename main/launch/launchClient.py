1851cb8a624a1da5ee5ac38055c0f2fb80fd09e1
Derick <dboehm2019@outlook.com>
2023-02-26 00:59:39 -0600

    Create test env and basic test

--- main/launch/launchClient.py
+++ main/launch/launchClient.py
@@ -15,5 +15,7 @@ def launch():
     except Exception as e:
         if str(e) == "invalid literal for int() with base 10: ''":
             return "You need to post stuff and thangs"
+        elif str(e) == "400 Bad Request: The browser (or proxy) sent a request that this server could not understand.":
+            return "this right here"
 
         return json.dumps({'errorMessage': str(e)})
