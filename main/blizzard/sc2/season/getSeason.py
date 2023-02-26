27a4e8a064d9bb062e957cdeef5b58584a6d32e8
Derick <dboehm2019@outlook.com>
2023-02-26 15:01:09 -0600

    Flask up getSeason

--- main/blizzard/sc2/season/getSeason.py
+++ main/blizzard/sc2/season/getSeason.py
@@ -1,22 +1,30 @@
-import requests
-import json
+from flask import Blueprint, json
 import os
+import requests
 
-thisDir = os.path.dirname(__file__)
-authDir = thisDir[:55] + "OAuth\\data.json"
+cseason = Blueprint('cseason', __name__)
 
-f = open(authDir)
-data = json.load(f)
-token = data["access_token"]
 
-# TODO make this a post variable sent in the request
-# options are 1=US 2=EU 3=KO and TW 5=CN
-region = "1"
+@cseason.route('/getseason')
+@cseason.route('/')
+def season():
+    thisDir = os.path.dirname(__file__)
+    authDir = thisDir[:55] + "OAuth\\data.json"
 
-url = "https://us.api.blizzard.com/sc2/ladder/season/" + str(region) + "?access_token=" + token
+    f = open(authDir)
+    data = json.load(f)
+    token = data["access_token"]
 
-response = requests.get(url)
-data = response.json()
+    # TODO make this a post variable sent in the request
+    # options are 1=US 2=EU 3=KO and TW 5=CN
+    region = "1"
 
-with open("data.json", "w") as f:
-    json.dump(data, f)
+    url = "https://us.api.blizzard.com/sc2/ladder/season/" + str(region) + "?access_token=" + token
+
+    response = requests.get(url)
+    data = response.json()
+
+    with open(thisDir + "\data.json", "w") as f:
+        json.dump(data, f)
+
+    return data
