45e30ce2d6cb41965fee66f33582b47aee71015c
Derick <dboehm2019@outlook.com>
2023-02-26 20:18:28 -0600

    Fix tests

--- runTest.py
+++ runTest.py
@@ -4,13 +4,18 @@ from test.blizzard import *
 
 # Run the app in a container
 runDocker()
+fail = False
 
 # Run the tests
 try:
-    checkResult("Docker build works", dockerTest())
+    dockerTest()
     test_getSeason()
 except Exception as e:
     print(e)
+    fail = True
 
 # kill and delete the container
 killDocker()
+
+if not fail:
+    print("Pass")
